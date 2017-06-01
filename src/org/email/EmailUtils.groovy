35ea640e429da0f9fa749fd73a7ff3221e4d47a3
Kanagaraj Kandasamy <Kanagaraj.Kandasamy@xome.com>
2017-06-01 23:06:17 +0530

    added Email Utility.

---
+++ src/org/email/EmailUtils.groovy
@@ -0,0 +1,19 @@
+class EmailUtils {
+    def sendEmail(recipientsList,subject, mimeType, body,attachFilePath) {
+	    println("attachement file path=$attachFilePath")
+		emailext 	attachmentsPattern: attachFilePath, 
+					body: body, 
+					mimeType: mimeType, 
+					subject: subject, 
+					to: recipientsList.join(",")
+    }
+
+    def getEmailSubject( status) {
+	    return  " $status : $PROJ_NAME - ${GIT_HASH} - ${env.JOB_NAME} - ${env.BUILD_NUMBER} " 
+    }
+
+    def getEmailBody( status) {
+	    println( "'${env.JOB_NAME} - ${env.BUILD_NUMBER} - ${GIT_HASH}'" )
+        return "<p> $status: Job '${env.JOB_NAME} - ${env.BUILD_NUMBER} - ${GIT_HASH}':</p> <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"
+    }
+}
