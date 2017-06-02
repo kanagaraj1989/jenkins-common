142e00c9bbb5aa11f143e3a554e44a54cd444224
CORP\kkandasa <kanagaraj.kandasamy>
2017-06-02 11:29:29 +0530

    passing this reference-1.

--- src/org/email/EmailUtils.groovy
+++ src/org/email/EmailUtils.groovy
@@ -4,9 +4,13 @@ class EmailUtils implements Serializable  {
 	private def script
 	private def proj_name
 	private def git_hash
-	
+	private def subject
+	private def body
+	private def mimeType
+
 	EmailUtils(script) {
 		this.script = script
+		this.mimeType = 'text/plain'
 	}
 	
 	def setProjectName(proj_name) {
@@ -16,22 +20,26 @@ class EmailUtils implements Serializable  {
 	def setGitHash(git_hash) {
 		this.git_hash = git_hash
 	}
+    
+	def setMimeType(mimeType) {
+		this.mimeType = mimeType
+	}
 
-    def sendEmail(recipientsList,subject, mimeType, body,attachFilePath) {
+    def sendEmail(recipientsList,attachFilePath) {
 	    println("attachement file path=$attachFilePath")
 		emailext 	attachmentsPattern: attachFilePath, 
-					body: body, 
-					mimeType: mimeType, 
-					subject: subject, 
+					body: this.body, 
+					mimeType: this.mimeType, 
+					subject: this.subject, 
 					to: recipientsList.join(",")
     }
 
-    def getEmailSubject( status) {
-		 return  " $status : ${this.proj_name} - ${this.git_hash} - ${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} " 
+    def setEmailSubject( status) {
+		 this.subject = "$status : ${this.proj_name} - ${this.git_hash} - ${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} " 
     }
 
-    def getEmailBody( status) {
+    def setEmailBody( status) {
 	    println( "'${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.git_hash}'" )
-        return "<p> $status: Job '${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.git_hash}':</p> <p>Check console output at &QUOT;<a href='${this.script.env.BUILD_URL}'>${this.script.env.JOB_NAME} [${this.script.env.BUILD_NUMBER}]</a>&QUOT;</p>"
+        this.body = "<p> $status: Job '${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.git_hash}':</p> <p>Check console output at &QUOT;<a href='${this.script.env.BUILD_URL}'>${this.script.env.JOB_NAME} [${this.script.env.BUILD_NUMBER}]</a>&QUOT;</p>"
     }
 }
