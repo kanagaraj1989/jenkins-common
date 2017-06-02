9159ea81e8139b76e3bdd7086b63198c529ef769
CORP\kkandasa <kanagaraj.kandasamy>
2017-06-02 11:14:30 +0530

    passing this reference.

--- src/org/email/EmailUtils.groovy
+++ src/org/email/EmailUtils.groovy
@@ -1,10 +1,22 @@
 package org.email;
 
 class EmailUtils implements Serializable  {
-	def script
+	private def script
+	private def proj_name
+	private def git_hash
+	
 	EmailUtils(script) {
 		this.script = script
 	}
+	
+	def setProjectName(proj_name) {
+		this.proj_name = proj_name
+	}
+
+	def setGitHash(git_hash) {
+		this.git_hash = git_hash
+	}
+
     def sendEmail(recipientsList,subject, mimeType, body,attachFilePath) {
 	    println("attachement file path=$attachFilePath")
 		emailext 	attachmentsPattern: attachFilePath, 
@@ -15,11 +27,11 @@ class EmailUtils implements Serializable  {
     }
 
     def getEmailSubject( status) {
-		 return  " $status : ${this.script.proj_name} - ${this.script.git_hash} - ${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} " 
+		 return  " $status : ${this.proj_name} - ${this.git_hash} - ${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} " 
     }
 
     def getEmailBody( status) {
-	    println( "'${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.script.git_hash}'" )
-        return "<p> $status: Job '${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.script.git_hash}':</p> <p>Check console output at &QUOT;<a href='${this.script.env.BUILD_URL}'>${this.script.env.JOB_NAME} [${this.script.env.BUILD_NUMBER}]</a>&QUOT;</p>"
+	    println( "'${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.git_hash}'" )
+        return "<p> $status: Job '${this.script.env.JOB_NAME} - ${this.script.env.BUILD_NUMBER} - ${this.git_hash}':</p> <p>Check console output at &QUOT;<a href='${this.script.env.BUILD_URL}'>${this.script.env.JOB_NAME} [${this.script.env.BUILD_NUMBER}]</a>&QUOT;</p>"
     }
 }
